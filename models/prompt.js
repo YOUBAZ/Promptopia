fdfc8efbaeabf10d9ec9cf2d0a5b11312d7ee057
Yousef Bakr Zaki Alsaidi <youbakrzaki@gmail.com>
2024-08-03 21:37:15 +0300

    Read All Prompt Functionality

--- models/prompt.js
+++ models/prompt.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model, models } from "mongoose";
 const PromptSchema = new Schema({
   creator: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: Schema.Types.ObjectId,
     ref: "User",
   },
   prompt: {
